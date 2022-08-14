e190a636d8e2c3584e5ecd236ccd0449b3df83e8
ofuji-works <ofuji.works@gmail.com>
2022-08-15 02:09:33 +0900

    guessing_game 入力された値の表示

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,28 @@
+// ライブラリをスコープに入れる
+// stdと呼ばれる標準ライブラリの中のio（入出力）ライブラリ
+// Rustはデフォルトで、標準ライブラリで定義されているアイテム(prelude（プレリュード）)
+// 使いたい型がpreludeにない場合は、その型をuse文で明示的にスコープに入れる必要があります。
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+
+    // Rustでは変数はデフォルトで不変（immutable）になります。
+    // 変数を可変（mutable）にするには、変数名の前にmutをつけます。
+    // Stringは標準ライブラリによって提供される文字列型で、サイズが拡張可能な、UTF-8でエンコードされたテキスト片になります。
+    let mut guess = String::new();
+
+    // もし、プログラムの最初にuse std::ioと書いてioライブラリをインポートしていなかったとしても、std::io::stdinのように呼び出せば、この関数を利用できます。 
+    io::stdin()
+        // read_lineの引数として&mut guessを渡し、ユーザ入力をどの文字列に格納するかを指示しています。
+        // この&は、この引数が参照であることを示し、これによりコードの複数の部分が同じデータにアクセスしても、そのデータを何度もメモリにコピーしなくて済みます。
+        // 変数のように参照もデフォルトで不変
+        // したがって、&guessではなく&mut guessと書いて可変にする必要があります。
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess); 
+}
